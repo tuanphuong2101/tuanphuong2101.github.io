e09d4ecd7f18857bad632b2ff7c5efbdad013d72
tuanphuong2101 <46560290+tuanphuong2101@users.noreply.github.com>
2019-01-12 00:23:10 +0700

    Add files via upload

--- 1.js
+++ 1.js
@@ -5,18 +5,28 @@
 	 new WOW().init();
 	 var hu=new TimelineMax();
 
-	 function chay(){
-	 	hu.to($('.quatim'),0.4,{scale:0.9}); 
-	 	hu.to($('.quatim'),0.4,{scale:1});
-	 	chay();
-	 }
-	 chay();
+	 // function chay(){
+	 // 	hu.to($('.quatim'),0.4,{scale:0.9}); 
+	 // 	hu.to($('.quatim'),0.4,{scale:1});
+	 // 	chay();
+	 // }
+	 // chay();
 
 	 // hu.to($('.tenck'),1,{x:-150});
 	 // hu.to($('.tenvk'),1,{x:-135});
-	 TweenMax.staggerTo($('.tenck'),1,{x:-130,opacity:1},1);
-	 TweenMax.staggerTo($('.tenvk'),1,{x:-110,opacity:1},1)
-	})  
+	 // 
+	 // 
+	 // TweenMax.staggerTo($('.tenck'),1,{x:-110,opacity:1},1);
+	 // TweenMax.staggerTo($('.tenvk'),1,{x:-100,opacity:1},1)
+
+	 // codecho responsive
+	 
+	 var manhinhdienthoai=$(window).width();
+	 if (manhinhdienthoai<=400) {
+	 	$('.col-lg-2.gachgiua.col-lg-push-1').addClass('xoagach');
+	 }
+	})
+
  
 
 
---
+++ 2.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",function(){
+	// var click1=document.getElementsByTagName('navbar-toggle');
+	// var click=click1[0];
+	// console.log(click)
+	// click.onclick=function(){
+
+	// };
+},false)
